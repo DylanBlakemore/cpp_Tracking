813968880927cab5d9abe2062e3946066d9cb99e
Dylan Blakemore <dylan.blakemore@gmail.com>
2016-07-28 15:46:57 +0200

    Can now load input file and create TrackEntry objects.

--- include/TrackEntry.h
+++ include/TrackEntry.h
@@ -10,17 +10,42 @@
 
 #include "../include/Vec3d.h"
 
+#include <sstream>
+#include <stdlib.h>
+
 class TrackEntry {
 public:
-	TrackEntry();
+	TrackEntry() {
+		x = Vec3d(0,0,0);
+		t = 0;
+	}
 	TrackEntry(const std::string& in_string) {
 		// in_string is a line in the csv input file. Parse the string into
 		// a Vec3d object and a time float.
+		std::stringstream ss(in_string);
+		std::string s_value;
+		double x_vals[3];
+		unsigned int dim = 0;
+
+		while(std::getline(ss, s_value, ',')) {
+			if(dim > 3)
+				break;
+			else if(dim == 3)
+				t = atof(s_value.c_str());
+			else
+				x_vals[dim] = atof(s_value.c_str());
+			dim++;
+		}
+
+		x = Vec3d(x_vals);
 	}
 	TrackEntry(const Vec3d& in_vec, const double in_t) {
 		x = in_vec;
 		t = in_t;
 	}
+
+	Vec3d getPosition() const {return x;}
+	double getTimestamp() const {return t;}
 
 private:
 	Vec3d x;
--- src/cpp_Tracking.cpp
+++ src/cpp_Tracking.cpp
@@ -7,12 +7,41 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <fstream>
 
 #include "../include/Vec3d.h"
-using namespace std;
+
+/* Returns a vector of strings, with each entry being a line of the input csv file.
+ *
+ * 'filename' is the path to the csv file.
+ */
+std::vector<std::string> loadCSV(const std::string& filename) {
+	std::ifstream csv_file(filename);
+
+	std::string line;
+	std::vector<std::string> line_vec;
+
+	while(std::getline(csv_file, line)) {
+		line_vec.push_back(line);
+	}
+
+	return line_vec;
+}
+/* Returns result of command line input.
+ */
+std::string getInput(const std::string prompt) {
+	std:: cout << prompt;
+	std::string input_str;
+	std::cin >> input_str;
+
+	return input_str;
+}
 
 int main() {
-
+	std::string input_fname = getInput("Enter the path to the input .csv file: ");
+	std::vector<std::string> csv_line_vec = loadCSV(input_fname);
 
 	return 0;
 }
